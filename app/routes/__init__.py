2b92bcb5e650d30862175bb333e692fb280fc17b
Mustafa Saifee <saifeemustafaq@gmail.com>
2024-12-04 13:03:53 +0530

    Create __init__.py

---
+++ app/routes/__init__.py
@@ -0,0 +1,6 @@
+from .user_routes import user_bp
+from .profile_routes import profile_bp
+from .activity_routes import activity_bp
+from .main_routes import main_bp
+
+__all__ = ['user_bp', 'profile_bp', 'activity_bp', 'main_bp'] 
